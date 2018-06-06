a29cba1a58df5a7e7fa4a1bf7b88b29cffacec82
James Tien <lyisse@sina.com>
2018-06-06 23:43:26 +0800

    我的第三次提交

---
+++ test14.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  6 11:11:47 2018
+
+@author: X270
+"""
+
+p = 640
+q = 49
+i = p * q
+print(i)
+e = 16.3
+pe = i / e
+print(pe)
+
+c = 12
+o = 16
+co2 = c + o * 2
+print(co2)
+
+t = 49 / c * co2
+print(t)
